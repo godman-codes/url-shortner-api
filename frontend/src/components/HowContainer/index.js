28294aff3ecf9335afeb467d9c27ca04ac3d81a7
Faseun <faseunoluwatobilobasimon@gmail.com>
2022-10-31 20:05:47 +0100

    finished with the how section

--- frontend/src/components/HowContainer/index.js
+++ frontend/src/components/HowContainer/index.js
@@ -1,38 +1,50 @@
 import React from "react";
 import { Wrapper, Content } from "./styles";
+import How1 from "../../images/how1.png";
+import How2 from "../../images/how2.png";
+import How3 from "../../images/how3.png";
+
+// import Circle1 from "../../images/circle1.png";
+// import Circle2 from "../../images/circle2.png";
 
 const HowContainer = () => {
    return (
       <Wrapper>
+         {/* <div className="ball-1">
+            <img src={Circle2} alt="circle-2" />
+         </div>
+         <div className="ball-2">
+            <img src={Circle1} alt="circle-1" />
+         </div> */}
          <Content className="container">
             <div className="section-header">
                <h2 className="title">How to start</h2>
             </div>
             <div className="section-row">
-               <div className="section-row-item">
+               <div className="section-col">
                   <div className="how-item">
                      <div className="how-thumb">
-                        <img src="" alt="" />
+                        <img src={How1} alt="how" />
                      </div>
                      <div className="how-content">
                         <h6 className="title">CREATE AN ACCOUNT</h6>
                      </div>
                   </div>
                </div>
-               <div className="section-row-item">
+               <div className="section-col">
                   <div className="how-item">
                      <div className="how-thumb">
-                        <img src="" alt="" />
+                        <img src={How2} alt="how-2" />
                      </div>
                      <div className="how-content">
                         <h6 className="title">SHORTEN YOUR LINK</h6>
                      </div>
                   </div>
                </div>
-               <div className="section-row-item">
+               <div className="section-col">
                   <div className="how-item">
                      <div className="how-thumb">
-                        <img src="" alt="" />
+                        <img src={How3} alt="how-3" />
                      </div>
                      <div className="how-content">
                         <h6 className="title">EARN MONEY</h6>
--- frontend/src/components/HowContainer/styles.js
+++ frontend/src/components/HowContainer/styles.js
@@ -14,6 +14,18 @@ export const Wrapper = styled.div`
       padding-top: 60px;
       padding-bottom: 120px;
    }
+   /* .ball-1 {
+      transform: unset;
+      transition: transform 0.1s ease 0s;
+      will-change: transform;
+      top: 0;
+      right: 40%;
+      opacity: 1;
+      position: absolute;
+      @media (min-width: 992px) {
+         display: block !important;
+      }
+   } */
 `;
 export const Content = styled.div`
    .section-header {
@@ -25,6 +37,51 @@ export const Content = styled.div`
       }
       title {
          margin-bottom: 0 !important;
+      }
+   }
+   .section-row {
+      margin-bottom: -40px;
+      justify-content: center !important;
+      display: flex;
+      flex-wrap: wrap;
+      margin-right: -15px;
+      margin-left: -15px;
+      .section-col {
+         position: relative;
+         width: 100%;
+         padding-right: 15px;
+         padding-left: 15px;
+         @media (min-width: 576px) {
+            flex: 0 0 83.333333%;
+            max-width: 83.333333%;
+         }
+         @media (min-width: 768px) {
+            flex: 0 0 50%;
+            max-width: 50%;
+         }
+         @media (min-width: 992px) {
+            flex: 0 0 33.333333%;
+            max-width: 33.333333%;
+         }
+         .how-item {
+            text-align: center;
+            margin-bottom: 32px;
+            .how-thumb {
+               margin: 0 auto 25px;
+               @media (min-width: 576px) {
+                  margin-bottom: 40px;
+               }
+               img {
+                  max-width: 100%;
+               }
+            }
+            .how-content {
+               text-transform: uppercase;
+               @media (min-width: 576px) {
+                  font-size: 22px;
+               }
+            }
+         }
       }
    }
 `;
